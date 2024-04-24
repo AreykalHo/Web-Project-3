59b2da17fed77e4ecf229a6e4a4adf1434a6400b
SamnangGit <phengsamnanggit@gmail.com>
2024-04-24 11:01:23 +0700

    initial commit

---
+++ src/test/java/com/bakery/shop/bakeme/BakemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakery.shop.bakeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
