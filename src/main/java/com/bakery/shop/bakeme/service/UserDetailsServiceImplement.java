2c18e0e874123b1da8202807b1b71afd851a5de8
SamnangGit <phengsamnanggit@gmail.com>
2024-04-28 13:35:11 +0700

    progress on login

---
+++ src/main/java/com/bakery/shop/bakeme/service/UserDetailsServiceImplement.java
@@ -0,0 +1,24 @@
+package com.bakery.shop.bakeme.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.bakery.shop.bakeme.repository.UserRepository;
+
+public class UserDetailsServiceImplement implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+        return new UserDetailsServices(user);
+    }
+}
