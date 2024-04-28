088983e3c2b33eb30fec2af1a3db47d390bd4d7a
Khunsopagna Keo <2022173keo@aupp.edu.kh>
2024-04-28 11:00:53 +0700

    updating AppController

--- src/main/java/com/bakery/shop/bakeme/controller/AppController.java
+++ src/main/java/com/bakery/shop/bakeme/controller/AppController.java
@@ -1,3 +1,39 @@
+package com.bakery.shop.bakeme.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AppController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home"; 
+    }
+
+    @GetMapping("/shop")
+    public String shop() {
+        return "shop"; 
+    }
+
+    @GetMapping("/cart")
+    public String cart() {
+        return "cart"; 
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact"; 
+    }
+
+    @GetMapping("/team")
+    public String team() {
+        return "team"; 
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; 
+    }
     
 }
